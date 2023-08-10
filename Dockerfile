bcf68dd0cd59a22da8fe28801286d76734b8bf79
Romero Antonio Ramos de Mendonça <romeromendonca22@gmail.com>
2023-08-10 17:52:09 -0300

    Initialized Project

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM openjdk:17
+
+WORKDIR /app
+
+COPY /build/libs/pink_project-0.0.1-SNAPSHOT-plain.jar /app/pink_project.jar
+
+EXPOSE 8080
+
+CMD java -jar pink_project.jar
---
+++ src/main/java/com/example/pink_project/PinkProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.pink_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinkProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PinkProjectApplication.class, args);
+    }
+
+}
