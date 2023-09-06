9748e3507eced6303b5928f916be0dd88a9e3ba6
Romero Antonio Ramos de Mendonça <romeromendonca22@gmail.com>
2023-09-06 01:17:10 -0300

    Removed unnused service, dto, implementations and configurations; Changed build.gradle dependencies; Changed application.properties and PinkProjectApplication.java

--- src/main/java/com/example/pink_project/PinkProjectApplication.java
+++ src/main/java/com/example/pink_project/PinkProjectApplication.java
@@ -1,6 +1,5 @@
 package com.example.pink_project;
 
-import com.example.pink_project.config.AppConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -11,8 +10,5 @@ public class PinkProjectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PinkProjectApplication.class, args);
-        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
-        ctx.register(AppConfig.class);
-        ctx.refresh();
     }
 }
--- src/main/java/com/example/pink_project/repositories/ProductRepository.java
+++ src/main/java/com/example/pink_project/repositories/ProductRepository.java
@@ -2,25 +2,11 @@ package com.example.pink_project.repositories;
 
 import com.example.pink_project.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.UUID;
 
 
 
 @Repository
-public interface ProductRepository {
-    List<Product> findAll();
-
-    void save(Product product);
-
-    Product findById(UUID id);
-
-    void updateStatus(UUID id, Product product);
-
-    void deleteById(UUID id);
-}
+public interface ProductRepository extends JpaRepository<Product, UUID> {}
